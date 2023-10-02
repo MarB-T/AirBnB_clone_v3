3d6805e317d1755538ff8cf124c596a3faab3651
MarB-T <danmarbet@gmail.com>
2023-10-02 21:22:17 +0300

    task three

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that register the blueprint app_views to your Flask instance app"""
+
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from flask_core import CORS
+from models import storage
+from os import getenv
+
+
+app = Flask(__name__)
+
+
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_engine(exc):
+    """remove current SQLAlchemy session"""
+    storage.close()
+
+
+
+@app.errorhandler(404)
+def not_found():
+    """Handles not found error"""
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
